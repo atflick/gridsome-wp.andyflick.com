c39ff3df30cdba3dcca739960bc8b3d6c7ac70ea
Andy Flickinger <andy@interactivestrategies.com>
2019-12-21 12:10:19 -0500

    working components loop, some hero data

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hero" :style="{ backgroundImage: `url(${image})`}">
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      title: this.$attrs.fields.title,
+      text: this.$attrs.fields.text,
+      image: this.$attrs.fields.image.url
+
+    }
+  }
+}
+</script>
--- src/templates/Default.vue
+++ src/templates/Default.vue
@@ -2,6 +2,7 @@
   <Layout>
     <h1>{{ $context.title }}</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <component v-for="(component, index) in components" :key="index" :is="component.componentName" :fields="component"></component>
   </Layout>
 </template>
 
@@ -9,6 +10,21 @@
 export default {
   metaInfo: {
     title: 'About us'
+  },
+  data() {
+    return {
+      // components: this.$context.fields.components
+    }
+  },
+  computed: {
+    components() {
+      return this.$context.fields.components.map((component) => {
+        console.log(component);
+        component.componentName = component.acf_fc_layout.replace(/_/g, '-');
+        return component;
+      });
+    }
+
   }
 }
 </script>
