5c11e9bcceda24c81532d2e4b06d629dde58a2ac
Andy Flickinger <atflick@gmail.com>
2020-12-19 14:08:37 -0500

    remove naimation

--- src/micros/parallax/ParallaxContainer.vue
+++ src/micros/parallax/ParallaxContainer.vue
@@ -185,7 +185,6 @@ export default {
       background-size: cover;
       background-position: center;
       background-repeat: no-repeat;
-      transition: ease .5ms;
       -webkit-transform-style: preserve-3d;
       -webkit-backface-visibility: hidden;
     }
